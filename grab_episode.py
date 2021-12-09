4130b3838a3e2c5a27f2c2a039f4fd02178866b3
Khoi Nguyen <1017154kn@gmail.com>
2021-12-08 22:49:52 -0800

    moved everything to root folder

---
+++ grab_episode.py
@@ -0,0 +1,200 @@
+#!usr/bin/python3
+"""
+# Authors: shazelquist
+#
+# Directive: Grab information regarding episode & season data, parse into a format to populate
+#
+"""
+# Authors
+# -shazelquist
+
+# Notes:
+# -Full linting style and document not provided
+# -Checks for 'WIKIPATH' environment variable, set in advance
+
+# Imports:
+import re # regular expressions
+import requests # request webpage if not present
+from .model import * # grab model
+from os import listdir, environ # compare to files, enviornment variables
+from os.path import isfile # check for existing file
+from bs4 import BeautifulSoup as BS # Pase information out of htmlpage
+
+
+# Manage parsing from soup into friendlier source
+# -Parses from web soup and returns info as dictionary
+# -nonstandard imports to keep non-calls from breaking
+# -returns dictionary {filename:{'Name':,'Season':,'Episode':,'Minor':}}
+def get_episode_info(filelist:list,logging=True):
+    """parse soup into episode information into easy structure {filename:{'Name':,'Season':,'Episode':,'Minor':}}. Takes (['list of avaliable transcript files'], True/False/None logging argument)"""
+
+    # set up information regarding filenames
+    filelink={fl.lower():fl for fl in filelist}# list of filenames
+    linkeys=list(filelink.keys())
+    soup=get_season_data()# retrieve soup either from file or web
+
+    episodes={}#Episode name:(season, episode number, minor)
+    if logging:# wipe current parsed debug file
+        with open('parsed.txt','w', encoding='utf-8') as parsefile:
+            parsefile.write('')
+    season=0# season counter
+    eplog=''# logging variable
+    mtc=0# logging variable
+    missing_transcripts=''# logging variable
+
+    for tag in soup.find_all('th',id=re.compile("ep[0-9]+[a-z]{0,1}")):#find all episodes
+        ep=tag.find_next_sibling(style='text-align:center')
+        if ep.text=='1a':# Increment counter instead of searching, this method is easier
+            season+=1
+        if not ep.text[-1].isalpha():# check for those without minor and assign default
+            minor=1 #a
+            episode=int(ep.text)
+        else:
+            episode=int(ep.text[:-1])# seperate major from minor
+            minor=ord(ep.text[-1])-96 # retrieve minor and convert to int
+        name=ep.find_next_sibling('td').text[1:-1]
+        # replace Digits, Cap first, ascii-fiy
+        # probably a better way to write this expression
+        fname=re.sub(r'[0-9\s.,\-:;?!"\']','',name).replace("&",'amp')# delete and replace characters
+        fname=fname[0].capitalize()+fname[1:]# capitalize
+
+        # fix manually check & fix known errors, should replace this in the future
+        fname,upd=man_fix_parsing_errors(fname,tag)
+
+        if logging:# write parsed entries to file for debugging
+            eplog+=tag.text+'\n' # save episode code
+            with open('parsed.txt','a', encoding='utf-8') as parsefile:
+                parsefile.write('{}\n'.format(fname))
+
+        if fname.lower()+'.txt' in linkeys:# reverse query to avoid key error
+            fname=filelink[fname.lower()+'.txt']
+            episodes[fname]={'Name':name,'Season':season,'Episode':episode,'Minor':minor}
+            episodes[fname].update(upd)# apply update for fixups
+        elif logging:
+            mtc+=1
+            missing_transcripts+='#{} : {} -> {}\n'.format(tag.text,name,fname)
+
+    if logging: # write episode codes to debug
+        with open('eplog.txt','w') as epl:
+            epl.write(eplog)
+
+    # manually add a special episode, not fit for model, but file is present
+    episodes['SpongeBobsBigBirthdayBlowout.txt']={'Name':"SpongeBob's Big Birthday Blowout",'Season':12,'Episode':9,'Minor':"S"}
+
+    if logging and mtc:# If there are missing transcripts, write them down
+        mtfile='missing_transcripts.txt'
+        print('{} missing transcripts with current parsing, wrote to {}'.format(mtc,mtfile))
+        with open(mtfile,'w',encoding='utf-8') as missingt:
+            missingt.write(missing_transcripts)
+
+    return episodes
+
+def man_fix_parsing_errors(filename,tag):#
+    """Update infomation for entries that does not conform to regular grid standard or name conversion. Takes(filename, soup tag)"""
+    update={}#filename:{'Name':,'Season':,'Episode':,'Minor':
+    if tag.text=='123124': # two part episode, one file
+        update['Name']='Truth or Square'
+        update['Episode']=23
+        update['Minor']=1
+        filename='TruthorSquare'
+    elif tag.text=='126':# two titles
+        filename='TheClashofTriton'
+        update['name']='The Clash of Triton'
+    elif tag.text=='266':# two titles
+        filename='EscapefromGloveWorld'
+        update['name']='Escape from Glove World'
+    elif tag.text=='98':# two titles
+        filename='WhatEverHappenedtoSpongeBob'
+        update['name']="What Ever Happened to SpongeBob?"
+    elif tag.text=='142a':#Non-standard filename
+        filename='Trenchbilles'
+    elif tag.text=='149a':# two titles
+        filename='YouDontKnowSpongeBob'
+    elif tag.text=='111':# two titles
+        filename="SpongeBobSquarePantsvstheBigOne"
+        update['name']="SpongeBob SquarePants vs the Big One"
+    elif tag.text=='85b':# Non-standard filename:utf-8 -> ascii
+        filename='KrabslaMode'
+    elif tag.text=='174b':# Non-standard filename
+        filename='ForHeretoGo'
+    elif tag.text=='237':# two titles
+        update['name']='SpaceBob MerryPants'
+        filename='GoonsontheMoon'
+        update['name']='Goons on the Moon'
+    elif tag.text=='61a':# parse error
+        filename='FearofaKrabbyPatty'
+        update['name']='Fear of a Krabby Patty'
+        update['Season']=4
+        update['episode']=1
+        update['minor']=1
+    return filename, update
+
+
+
+# Manage retrieval from html source
+# -reads information from a wiki page and writes to file
+# -reads file and returns soup for populating
+# -nonstandard imports to keep non-calls from breaking
+def get_season_data():
+    """retrieve information from the wiki, copy html to file, and soupify it. Returns BS4 soup"""
+
+    if not isfile('wikidata.html') or input('Force pull from web? y/n? ')=='y':
+        
+        # Perform user checkup
+        print("NOTE:\n-You must agree not to abuse this module, this pulls external information.")
+        print("-You will not make multiple pull requests\n-You will not place pulled information in any repo")
+        if input('Please type "I agree" in order to continue: ').lower()!='I agree'.lower():
+            print('Exiting, user did not agree to requirements')
+            return
+
+        if 'WIKIPATH' in environ:# ask for wiki url if not in enviornment variables
+            url=environ['WIKIPATH']
+        else:
+            url=input("Please enter the wiki/list_of_episodes source url path:").strip()
+        print('Requesting from "{}"'.format(url))
+        page=requests.get(url)
+        with open('wikidata.html','wb') as writefile:
+            writefile.write(page.content)
+
+    print('Attempting to load given copy')
+    with open('wikidata.html','rb') as source:
+        html=source.read()
+        print('wrote to wikidata.html')
+
+    souped=BS(html,'html.parser')
+    return souped
+
+def createobjects(files, ep_data):
+    """Create sqlalchemy modeled episode objects from episode data, ([files],{episode data}), returns list of objects"""
+    #__init__(self,season,episode,minor,name,filename):
+    objects=[]
+    for f in files:# only create for known files
+        ed=ep_data[f]
+        objects.append(episodes(ed['Season'],ed['Episode'],ed['Minor'],ed['Name'],f))
+    return objects # use create_all method
+
+def main():
+    """Main function in grab_episode, provides some debug functionality"""
+    ep=get_episode_info(listdir("SpongeBob_SquarePants_Transcripts"),False)
+    with open('out.txt','w',encoding='utf-8') as out:
+        for k in ep:
+            out.write('{}\t->\t{}\n'.format(k,ep[k]))
+
+    keys=list(ep.keys())
+    keys.sort()
+
+    files=listdir("SpongeBob_SquarePants_Transcripts")
+    errors=0
+    print("Checking for errors: files -> parsed")
+    for t in files:
+        if t not in keys:
+            errors+=1
+            print("ERROR ON {}".format(t))
+    if input('Do you want to populate {} items (y/n)? '.format(len(ep)))=='y':
+        res=createobjects(files,ep)
+        db.session.add_all(res)
+        db.session.commit()
+        print('items pushed to database')
+
+if __name__=='__main__':
+    main()
---
+++ quote_search.py
@@ -0,0 +1,111 @@
+# Khoi Nguyen
+# 9/9/2021
+# work on retrieving user's input
+# parse and clean up text files 
+# populating databases
+# return the speaker of the quote using user's input and query
+
+#imports
+from .model import *
+import os
+
+# Function to parse text file into a dictionary
+#  -key is line number
+#  -value is a list of 2 values
+#  -value[0] is speaker
+#  -value[1] is quote
+#  -still have junks in text files
+#  -not yet finished, only parse one file
+def parseTextFile():
+    key = 0
+    transcript_dict = {}
+    # change local path depends on your file path
+    folder = r'SpongeBob_SquarePants_Transcripts'
+    for path, dirc, files in os.walk(folder):
+        for filename in files:
+            print(filename)
+            with open(path + os.sep + filename, encoding="utf8") as file:
+                for line in file:
+                    count = 0
+                    line.strip()
+                    for string in line:
+                        if string == ":":
+                            value = line.split(":", 1)
+                            value.append(filename)
+                            transcript_dict[key] = value
+                            key += 1
+                            break
+                        elif string == "[" and count == 0:
+                            key += 1
+                            break
+                        elif string == "\n":
+                            continue
+                        else:
+                            count += 1
+    return transcript_dict
+
+# Function to clean up junks from parsed files and return cleaned dictionary
+#  -remove any unwanted characters and strings
+#  -unfinished, still missing junk options
+def cleanDict(dict):
+    newdict = {}
+    for key, items in dict.items():
+        for x in items[1]:
+            if x == "\n":
+                string = items[1].strip("\n")
+                items[1] = string
+            elif x == "/":
+                string = items[1].strip("/")
+                items[1] = string
+        newdict[key] = items
+        for x in items[0]:
+            if x == '(':
+                string = items[0].split(' ') 
+                items[0] = string[0]
+                newdict[key] = items
+        else:
+            newdict[key] = items
+    return newdict
+
+# Function to populate characters table
+def populateDatabaseCharacters(dict):
+    print('start to populate character data')
+    for key, items in dict.items():
+        character = characters(items[0])
+        missing = characters.query.filter_by(name=items[0]).first()
+        if missing is None:
+            db.session.add(character)
+            print('populating character '+str(key))
+    db.session.commit()
+    print('populated characters')
+
+# Function to populate Quotes table
+def populateDatabaseQuotes(dict):
+    print('start to populate quote data')
+    for key, items in dict.items():
+        string = items[1]
+        character = characters.query.filter_by(name=items[0]).first()
+        episode = episodes.query.filter_by(ep_filename=items[2]).first()
+        quote = quotes(episode.id, key, character.id, string)
+        db.session.add(quote)
+        print('populating quote '+str(key))
+    db.session.commit()
+    print('populated quotes')
+
+# For testing
+# def main():
+    # Uncomment this and run it on first set up for testing.
+    # db.create_all()
+    # dict = parseTextFile()
+    # newdict = cleanDict(dict)
+    # populateDatabaseCharacters(newdict)
+    # populateDatabaseQuotes(newdict)
+
+    # print("Please enter a quote from spongebob:\n")
+    # user_input = input()
+    # result = quotes.query.filter(quotes.quote.contains(user_input)).all()
+    # for x in result:
+    #     print("Season: " + str(x.episode.season) + " ep: " + str(x.episode.episode) + " title: " + x.episode.ep_name + str(x.character.name) + ": " + x.quote)
+
+# if __name__=='__main__':
+#     main()
